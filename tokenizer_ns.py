e3f9a5136e85587ebc342ef78d38d1d89c8b5ebb
Zakedu <hijklmnop0q@gmail.com>
2023-02-20 08:46:19 +0900

    single file > multiple files in specific folder

--- tokenizer_ns.py
+++ tokenizer_ns.py
@@ -1,18 +1,34 @@
+import os
 import nltk
 import openpyxl
 
-# Load the text file
-with open('example_token.txt', 'r') as f:
-    text = f.read()
+# Set the path to the folder containing the text files
+folder_path = '/Users/jake/PycharmProjects/pythonProject/GitHUB/tokenizer'
 
-# Tokenize the text
-sentences = nltk.sent_tokenize(text)
-tokens = [sentence.split() for sentence in sentences]
-
-# Write the tokens to an Excel file
+# Initialize the workbook
 wb = openpyxl.Workbook()
-ws = wb.active
-for i, sentence in enumerate(sentences):
-    for j, token in enumerate(tokens[i]):
-        ws.cell(row=i+1, column=j+1, value=token)
+
+# Loop through each text file in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.txt'):
+        # Load the text file
+        with open(os.path.join(folder_path, filename), 'r') as f:
+            text = f.read()
+
+        # Tokenize the text
+        sentences = nltk.sent_tokenize(text)
+        tokens = [sentence.split() for sentence in sentences]
+
+        # Create a new worksheet for the text file
+        ws = wb.create_sheet(title=filename)
+
+        # Add the tokens to the worksheet
+        for i, sentence in enumerate(sentences):
+            for j, token in enumerate(tokens[i]):
+                ws.cell(row=i+1, column=j+1, value=token)
+
+# Remove the default sheet created by openpyxl
+wb.remove(wb['Sheet'])
+
+# Save the workbook to an Excel file
 wb.save('output.xlsx')
